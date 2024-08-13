a99c97c8342ef996396a692bb6a2869a21f3e7fd
DESKTOP-KK0QNHO\meren <merendmrcc@gmail.com>
2024-08-13 09:15:57 +0300

    Created "sequential_model_2" trained on merc.csv

--- pricePredict2.py
+++ pricePredict2.py
@@ -15,7 +15,6 @@ os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 raw_data = pd.read_csv("merc.csv")
 # print(raw_data.describe())
 
-########PREP
 # dropping non numeric attrs
 raw_data.drop(["model","fuelType","transmission"], inplace=True, axis=1)
 
@@ -28,8 +27,6 @@ iqrMin = q1 - (1.5*iqr)
 iqrMax = q3 + (1.5*iqr)
 clean_data = raw_data[~((raw_data["price"]<iqrMin) | (raw_data["price"]>iqrMax))]
 
-
-
 # sbn.displot(raw_data["price"])
 # sbn.displot(clean_data["price"])
 # sbn.boxplot(raw_data["price"])
@@ -41,6 +38,53 @@ clean_data = raw_data[~((raw_data["price"]<iqrMin) | (raw_data["price"]>iqrMax))
 
 clean_data = clean_data[clean_data["year"] != 1970]
 
+x_data = clean_data.drop("price", axis=1).values
+y_data = clean_data["price"].values
+
+x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.3, random_state=42)
+scaler = MinMaxScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
+
+# modelling
+model = Sequential()
+model.add(Dense(8,activation="relu"))
+model.add(Dense(8,activation="relu"))
+model.add(Dense(8,activation="relu"))
+model.add(Dense(8,activation="relu"))
+model.add(Dense(8,activation="relu"))
+model.add(Dense(1))
+model.compile(optimizer="adam", loss="mse")
+
+model.fit(x= x_train, y= y_train, batch_size=32 , validation_data=(x_test,y_test), epochs=300)
+loss_data = pd.DataFrame(model.history.history)
+
+preds = model.predict(x_test)
+predictDf = pd.DataFrame(preds, columns=["predictions"])
+predictDf["trueValues"] = y_test
+
+mae = mean_absolute_error(y_test, preds)
+mse = mean_squared_error(y_test, preds)
+
+print(predictDf)
+print(f"""
+MAE: {mae},
+MSE: {mse}      
+""")
+
+# model.save("sequential_model_2.h5")
+
+fig, axes = plt.subplots(1, 2, figsize=(18, 9))
+
+sbn.scatterplot(x="predictions", y="trueValues", data=predictDf, ax=axes[0])
+axes[0].set_title("Predictions vs True Values")
+axes[0].set_xlabel("Predictions")
+axes[0].set_ylabel("True Values")
+
+loss_data.plot(ax=axes[1])
+axes[1].set_title("Model Loss Over Epochs")
+axes[1].set_xlabel("Epoch")
+axes[1].set_ylabel("Loss")
 
 
 plt.show()
--- test.py
+++ test.py
@@ -1,9 +1,11 @@
+from tensorflow.keras.models import load_model, Sequential
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 import pandas as pd
+import numpy as np
 
-data = pd.DataFrame(range(100),columns=["sayilar",])
-print(data)
+model = load_model("sequential_model_2.h5", custom_objects={"mse": mean_squared_error})
+data = np.array([2016,6300,569,28,5.5]).reshape(1,-1)
+data = pd.DataFrame(data,columns=["year","mileage","tax", "mpg","engineSize"])
+tahmin = model.predict(data)
+print(tahmin)
 
-
-
-clData = data[~((data["sayilar"]<5) | (data["sayilar"]>90))]
-print(clData)
